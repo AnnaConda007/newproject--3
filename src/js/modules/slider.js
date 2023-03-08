da1fe9aa8887e17c0a44a2ce583f5c8c1c5b6754
Anna Hrulckova <annahrulkova@yandex.ru>
2023-03-08 17:29:48 +0400

    видео с ютуб

--- src/js/modules/slider.js
+++ src/js/modules/slider.js
@@ -6,36 +6,68 @@ export default class Slider {
         this.slidIndex = 1
     }
     showSlides(n) {
-        if(n>this.slides.length){
+        if (n > this.slides.length) {
             this.slidIndex = 1
+        }
+        if (n < 1) {
+            this.slidIndex = this.slides.length
+        }
+        try {
+            this.hanson.style.opacity = "0"
+            if (n === 3) {
+                this.hanson.classList.add("animated")
+                setTimeout(() => {
+                    this.hanson.style.opacity = "1"
+                    this.hanson.classList.add("slideInUp")
+                }, 3000);
+            } else {
+                this.hanson.classList.remove("slideInUp")
             }
-            if(n<1){
-             this.slidIndex = this.slides.length
-            }
-       Array.from(this.slides).forEach(slide => {
+        } catch (error) {
+
+        }
+
+
+
+        Array.from(this.slides).forEach(slide => {
             slide.style.display = "none"
         })
         this.slides[this.slidIndex - 1].style.display = "block" // за счет чего значение slidIndex меняется при переключении слайдов?
     }
-  
-  
+
+
     plusSlides(n) {
         this.showSlides(this.slidIndex += n)
     }
-  
-  
+
+
     render() {
+
+        try {
+            this.hanson = document.querySelector(".hanson")
+
+        } catch (error) {
+
+        }
+
         this.btns.forEach(btn => {
             btn.addEventListener("click", () => {
                 this.plusSlides(1)//? увеличивается индекс показываемого слайда и вызывает  showSlides 
             })
-  
-            btn.parentNode.previousElementSibling.addEventListener("click",(e)=>{
-  e.preventDefault()
-  this.slidIndex=1
-  this.showSlides(this.slidIndex)//? клик на логотип и возвращение к первому слайду
+
+            btn.parentNode.previousElementSibling.addEventListener("click", (e) => {
+                e.preventDefault()
+                this.slidIndex = 1
+                this.showSlides(this.slidIndex)//? клик на логотип и возвращение к первому слайду
             })
         })
         this.showSlides(this.slidIndex) //? повторно вызывает showSlides во время одного клика?
+
+
+
+
+
+
+
     }
-  }
+}
